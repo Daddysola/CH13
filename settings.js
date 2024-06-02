a7c7ecc0fdde8c4763d51d47582858d2b434355c
Freddy Phoenix Mills <118795606+anonphoenix007@users.noreply.github.com>
2024-06-02 15:51:16 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -49,7 +49,7 @@ global.autoswview = false //auto view status/story
 //v2 is link + image menu,
 //v3 is video menu,
 //v4 is call end menu
-global.typemenu = 'v4'
+global.typemenu = 'v1'
 
 //text bug
 global.xbugtex = {
